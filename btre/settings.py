c7ac64d6582635e2d3fc08e6c3b9b57062d673fd
Tyler Evans <tvans12351@gmail.com>
2020-01-20 19:42:42 -0500

    shitty-shit

--- btre/settings.py
+++ btre/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'btredb',
         'USER': 'postgres',
-        'PASSWORD': 'kodak1',
+        'PASSWORD': '',
         'HOST': 'localhost'
     }
 }
